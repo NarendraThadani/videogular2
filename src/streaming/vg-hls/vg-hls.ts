7281a27b2ad4bc074a48fa1bdb06a501703cfc23
narendra-x1Carbon <narendra@connectingdotsinfotech.com>
2018-04-13 22:13:03 +0530

    test event on error build12

--- src/streaming/vg-hls/vg-hls.ts
+++ src/streaming/vg-hls/vg-hls.ts
@@ -114,8 +114,13 @@ export class VgHLS implements OnInit, OnChanges, OnDestroy {
           // try to recover network error
             
             console.error("fatal network error encountered, try to recover");
-            //this.API.customErrorEvent.emit(data);
-            //this.API.getDefaultMedia().dispatchEvent(new CustomEvent(VgEvents.VG_VOLUME_CHANGE));
+            if(this.API!=null){
+                this.API.customErrorEvent.emit(data);
+
+                if(this.API.getDefaultMedia()!=null){
+                    this.API.getDefaultMedia().dispatchEvent(new CustomEvent(VgEvents.VG_VOLUME_CHANGE));
+                }
+            }
             window.dispatchEvent(new CustomEvent(VgEvents.VG_START_ADS, data));
             
             
